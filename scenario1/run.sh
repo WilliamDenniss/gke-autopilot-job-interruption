5413e5f6f3764342aaa67fdfca2a60f672e79a24
William Denniss <wdenniss@google.com>
2022-06-19 10:45:56 +1000

    Add PDB

--- scenario1/run.sh
+++ scenario1/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f shortjob.yaml
-kubectl apply -f shortjob2.yaml
-kubectl apply -f shortjob3.yaml
-kubectl apply -f shortjob4.yaml
+kubectl create -f .
 
-kubectl apply -f longjob.yaml
+
